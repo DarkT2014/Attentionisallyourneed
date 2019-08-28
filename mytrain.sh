0bda5eeeb7b42fd6773b6f8ba17bc9476e6f6fa4
DarkT2014 <lincheng37211@gmail.com>
2019-08-28 14:51:56 +0800

    2

---
+++ mytrain.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+DATA_DIR=/tmp/t2t_datagen/
+PARAM_SET=base
+MODEL_DIR=model_dir/model_subword_4096_$PARAM_SET
+
+# SOURCE_DIR=./test_data/source_data
+INPUT_VOCAB=$DATA_DIR/en_sub_word.vocab
+TARGET_VOCAB=$DATA_DIR/zh_sub_word.vocab
+
+
+export PYTHONPATH="$PYTHONPATH:${$PWD}/models"
+echo $PYTHONPATH
+pip install --user -r models/official/requirements.txt
+
+python models/official/transformer/transformer_main.py --data_dir=$DATA_DIR --model_dir=$MODEL_DIR  --input_vocab_file $INPUT_VOCAB --target_vocab_file $TARGET_VOCAB
+    
+
+    # --param_set=$PARAM_SET
