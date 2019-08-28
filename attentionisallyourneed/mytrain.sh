7b5d1e75dcbbc1f1d67f5bafe3c02efb25dc5bc2
DarkT2014 <lincheng37211@gmail.com>
2019-08-28 14:24:48 +0800

    2

--- attentionisallyourneed/mytrain.sh
+++ attentionisallyourneed/mytrain.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 DATA_DIR=/tmp/t2t_datagen/
-PARAM_SET='<base>set'
+PARAM_SET=base
 MODEL_DIR=model_dir/model_subword_4096_$PARAM_SET
 
 # SOURCE_DIR=./test_data/source_data
@@ -9,4 +9,7 @@ INPUT_VOCAB=$DATA_DIR/en_sub_word.vocab
 TARGET_VOCAB=$DATA_DIR/zh_sub_word.vocab
 
 
-python transformer/transformer_main.py --data_dir=$DATA_DIR --model_dir=$MODEL_DIR --param_set=$PARAM_SET --input_vocab_file $INPUT_VOCAB --target_vocab_file $TARGET_VOCAB
+python transformer/transformer_main.py --data_dir=$DATA_DIR --model_dir=$MODEL_DIR  --input_vocab_file $INPUT_VOCAB --target_vocab_file $TARGET_VOCAB
+    
+
+    # --param_set=$PARAM_SET
