910ef0fe490f508c8282da7d65742b0a8c166a21
DarkT2014 <lincheng37211@gmail.com>
2019-08-29 10:23:03 +0800

    2

--- attentionisallyourneed/mytrain.sh
+++ attentionisallyourneed/mytrain.sh
@@ -9,9 +9,14 @@ INPUT_VOCAB=$DATA_DIR/en_sub_word.vocab
 TARGET_VOCAB=$DATA_DIR/zh_sub_word.vocab
 
 
-# export PYTHONPATH="$PYTHONPATH:$PWD/models"
+# export PYTHONPATH="$PYTHONPATH:${PWD}/models"
 # echo $PYTHONPATH
 # pip install --user -r models/official/requirements.txt
+
+# cat "${DATA_DIR}/en_sub_word.vocab" \
+#     "${DATA_DIR}/zh_sub_word.vocab" \
+#   > "${DATA_DIR}/enzhfornewtf.vocab"
+# VOCAB_FILE=$DATA_DIR/enzhfornewtf.vocab
 
 python transformer/transformer_main.py --data_dir=$DATA_DIR --model_dir=$MODEL_DIR  --input_vocab_file $INPUT_VOCAB --target_vocab_file $TARGET_VOCAB
     
---
+++ mytrainforNewtf.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+DATA_DIR=/tmp/t2t_datagen/
+PARAM_SET=base
+MODEL_DIR=model_dir/model_subword_4096_$PARAM_SET
+
+# SOURCE_DIR=./test_data/source_data
+INPUT_VOCAB=$DATA_DIR/en_sub_word.vocab
+TARGET_VOCAB=$DATA_DIR/zh_sub_word.vocab
+
+
+export PYTHONPATH="$PYTHONPATH:${PWD}/models"
+echo $PYTHONPATH
+pip install --user -r models/official/requirements.txt
+# export PYTHONPATH="$PYTHONPATH:${PWD}/models"
+
+
+cat "${DATA_DIR}/en_sub_word.vocab" \
+    "${DATA_DIR}/zh_sub_word.vocab" \
+  > "${DATA_DIR}/enzhfornewtf.vocab"
+VOCAB_FILE=$DATA_DIR/enzhfornewtf.vocab
+
+
+python models/official/transformer/transformer_main.py --data_dir=$DATA_DIR --model_dir=$MODEL_DIR  --vocab_file $VOCAB_FILE
+    
+
+    # --param_set=$PARAM_SET
