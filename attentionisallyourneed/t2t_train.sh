c0e24094c724aabbd3056fb7225500c749092a4f
DarkT2014 <lincheng37211@gmail.com>
2019-08-30 13:27:23 +0800

    2

---
+++ attentionisallyourneed/t2t_train.sh
@@ -0,0 +1,15 @@
+PROBLEM=translate_ende_wmt32k
+MODEL=transformer
+HPARAMS=transformer_base_single_gpu
+
+DATA_DIR=/tmp/t2t_datagen
+TRAIN_DIR=$HOME/t2t_train/$PROBLEM/$MODEL-$HPARAMS
+
+mkdir -p $DATA_DIR $TMP_DIR $TRAIN_DIR
+
+t2t-trainer \
+  --data_dir=$DATA_DIR \
+  --problem=$PROBLEM \
+  --model=$MODEL \
+  --hparams_set=$HPARAMS \
+  --output_dir=$TRAIN_DIR
