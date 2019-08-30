4bc79906c154bef5d15389f4642f494df616a701
DarkT2014 <lincheng37211@gmail.com>
2019-08-30 13:32:43 +0800

    2

--- attentionisallyourneed/t2t_train.sh
+++ attentionisallyourneed/t2t_train.sh
@@ -7,9 +7,4 @@ TRAIN_DIR=$HOME/t2t_train/$PROBLEM/$MODEL-$HPARAMS
 
 mkdir -p $DATA_DIR $TMP_DIR $TRAIN_DIR
 
-t2t-trainer \
-  --data_dir=$DATA_DIR \
-  --problem=$PROBLEM \
-  --model=$MODEL \
-  --hparams_set=$HPARAMS \
-  --output_dir=$TRAIN_DIR
+t2t-trainer --data_dir=$DATA_DIR --problem=$PROBLEM --model=$MODEL --hparams_set=$HPARAMS --output_dir=$TRAIN_DIR
