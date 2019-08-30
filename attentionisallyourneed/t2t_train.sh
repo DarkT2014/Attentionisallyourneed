847a4aaf78e550b3b669f4f1dcd49224922a33eb
DarkT2014 <lincheng37211@gmail.com>
2019-08-30 13:37:00 +0800

    2

--- attentionisallyourneed/t2t_train.sh
+++ attentionisallyourneed/t2t_train.sh
@@ -1,15 +1,11 @@
 PROBLEM=translate_enzh_wmt32k
 MODEL=transformer
-HPARAMS=transformer_base
+HPARAMS=transformer_base\n
+
 
 DATA_DIR=/tmp/t2t_datagen
 TRAIN_DIR=$HOME/t2t_train/$PROBLEM/$MODEL-$HPARAMS
 
 mkdir -p $DATA_DIR $TMP_DIR $TRAIN_DIR
 
-t2t-trainer \
-  --data_dir=$DATA_DIR \
-  --problem=$PROBLEM \
-  --model=$MODEL \
-  --hparams_set=$HPARAMS \
-  --output_dir=$TRAIN_DIR
+t2t-trainer --data_dir=$DATA_DIR --problem=$PROBLEM --model=$MODEL --hparams_set=$HPARAMS --output_dir=$TRAIN_DIR
