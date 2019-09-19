d88438a901fdc6786747404ad627022c7a9f7678
林骋 <lincheng@linchengdeMacBook-Pro.local>
2019-09-19 10:22:32 +0800

    2

--- AI_Challenger_2018-master/Baselines/english_chinese_machine_translation_baseline/train/run.sh
+++ AI_Challenger_2018-master/Baselines/english_chinese_machine_translation_baseline/train/run.sh
@@ -13,10 +13,10 @@ TRAIN_DIR=/tmp/t2t_outputs/$PROBLEM/$MODEL-$HPARAMS
 mkdir -p $DATA_DIR $TMP_DIR $TRAIN_DIR
 
 # Generate data
-t2t-datagen \
-  --data_dir=$DATA_DIR \
-  --tmp_dir=$TMP_DIR \
-  --problem=$PROBLEM
+# t2t-datagen \
+#   --data_dir=$DATA_DIR \
+#   --tmp_dir=$TMP_DIR \
+#   --problem=$PROBLEM
 
 # Train
 # *  If you run out of memory, add --hparams='batch_size=2048' or even 1024.
